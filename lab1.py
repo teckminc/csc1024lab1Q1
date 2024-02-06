b37a5a9d7fa988f85c059fc8de282d9e2ad4b661
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-06 09:04:59 +0800

    Insert Questions
    Insert Question

--- lab1.py
+++ lab1.py
@@ -1,3 +1,8 @@
+'''
+Write a program that prompts the user to enter five test scores and then prints the average of the test scores.
+Function to Uses:
+float(), input(), print(), round
+'''
 def main():
 
     return 0
