03c7b8080f94807725c18c1c29f2d1b13404c905
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-06 09:12:05 +0800

    Update Questions
    required 2 decimal point.

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 '''
-Write a program that prompts the user to enter five test scores and then prints the average of the test scores.
+Write a program that prompts the user to enter five test scores and then prints the average of the test scores. Make sure round to 2 decimal point.
 Function to Uses:
 float(), input(), print(), round
 '''
