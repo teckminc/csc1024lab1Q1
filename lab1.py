24822df1395606611204dab37053d5358177d1e8
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-06 09:15:42 +0800

    update questions
    update questions

--- lab1.py
+++ lab1.py
@@ -1,7 +1,8 @@
 '''
 Write a program that prompts the user to enter five test scores and then prints the average of the test scores. Make sure round to 2 decimal point.
 Function to Uses:
-float(), input(), print(), round
+float(), input(), print(), round()
+Operator: + , =
 '''
 def main():
 
